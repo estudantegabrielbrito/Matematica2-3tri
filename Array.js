6437cc74b97c21131a9836938dcc81080e2987d6
estudantegabrielbrito <gabriel.brito21@escola.pr.gov.br>
2026-09-11 11:57:53 -0300

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,11 @@
+let fruta = "maça"; 
+let frutas = ["maça","pera","abacate"];
+console.log(fruta);
+console.log(frutas);
+
+let numeros = [];
+numeros.push(4);
+numeros.push("melancia");
+
+console.log(numeros);
+console.log(frutas[1]);
