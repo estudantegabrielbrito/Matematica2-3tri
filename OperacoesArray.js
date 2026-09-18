06246df2755f1b4d67268a54670fccf8b9750320
estudantegabrielbrito <gabriel.brito21@escola.pr.gov.br>
2026-09-18 11:30:52 -0300

    Create OperacoesArray.js

---
+++ OperacoesArray.js
@@ -0,0 +1,14 @@
+let frutas = ["banana", "laranja", "maçã"];
+console.log(frutas);
+
+frutas.push("uva");
+console.log(frutas);
+
+frutas.unshift("morango");
+console.log(frutas);
+
+frutas.pop();
+console.log(frutas);
+
+frutas.shift();
+console.log(frutas);
